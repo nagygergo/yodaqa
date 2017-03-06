432c17dbc3dfbfa90de21488ee9a6c0e25961c94
Nagy Gergely <nagy.gergely14@gmail.com>
2017-03-06 21:38:57 +0100

    [WIP]

---
+++ src/main/java/cz/brmlab/yodaqa/analysis/question/ClueByWord2Vec.java
@@ -0,0 +1,60 @@
+package cz.brmlab.yodaqa.analysis.question;
+
+import org.apache.uima.UimaContext;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import cz.brmlab.yodaqa.provider.Word2VecHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import cz.brmlab.yodaqa.model.Question.Clue;
+import cz.brmlab.yodaqa.model.Question.ClueSV;
+import cz.brmlab.yodaqa.model.Question.SV;
+
+import java.io.File;
+import java.util.Collection;
+
+/**
+ * Generate Clue annotations in a QuestionCAS. These represent key information
+ * stored in the question that is then used in primary search.  E.g. "What was
+ * the first book written by Terry Pratchett?" should generate clues "first",
+ * "book", "first book", "write" and "Terry Pratchett".
+ *
+ * This just generates clues from SVs (selecting verbs). So it generates
+ * "write" for the above. */
+
+public class ClueByWord2Vec extends JCasAnnotator_ImplBase {
+    final Logger logger = LoggerFactory.getLogger(ClueByWord2Vec.class);
+    final static private int CLOSEST_WORD_COUNT = 10;
+    private Word2Vec vec;
+
+
+    public void initialize(UimaContext aContext) throws ResourceInitializationException {
+        vec = Word2VecHandler.getInstance();
+        super.initialize(aContext);
+    }
+
+    public void process(JCas jcas) throws AnalysisEngineProcessException {
+        for (Clue originClue  : JCasUtil.select(jcas, Clue.class)) {
+            Collection<String> closeStrings = vec.wordsNearest(originClue.getLabel(), CLOSEST_WORD_COUNT);
+            for(String string : closeStrings) {
+                addClue(jcas, originClue, string);
+            }
+        }
+    }
+
+    protected void addClue(JCas jcas, Annotation base, String label) {
+        Clue clue = new Clue(jcas);
+        clue.setBase(base);
+        clue.setWeight(1.0);
+        clue.setLabel(label);
+        clue.setIsReliable(false);
+        logger.debug("new by {}: {}", base.getType().getShortName(), clue.getLabel());
+    }
+}
---
+++ src/main/java/cz/brmlab/yodaqa/provider/Word2VecHandler.java
@@ -0,0 +1,66 @@
+package cz.brmlab.yodaqa.provider;
+
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+import org.deeplearning4j.models.embeddings.loader.WordVectorSerializer;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+
+
+public class Word2VecHandler {
+    private final static Logger logger = LoggerFactory.getLogger(Word2VecHandler.class);
+    private static Word2Vec instance = null;
+    private static final String CORPUS_FILE_ROUTE = "data/word2vec/GoogleNews-vectors-negative300.bin.gz";
+    private static final String CORPUS_URL = "https://drive.google.com/open?id=0B7XkCwpI5KDYNlNUTTlSS21pQmM";
+
+    public static Word2Vec getInstance() {
+        if(instance == null) {
+            File file = new File(CORPUS_FILE_ROUTE);
+            if(file.exists() && !file.isDirectory()){
+                instance = WordVectorSerializer.readWord2VecModel(file);
+                return instance;
+            } else {
+                downloadCorpus();
+                return getInstance();
+            }
+        } else {
+            return instance;
+        }
+    }
+
+    private static void downloadCorpus() {
+        URL url;
+        URLConnection con;
+        DataInputStream dis;
+        FileOutputStream fos;
+        byte[] fileData;
+        try {
+            logger.log("Corpus not found. Downloading googleNews-word2vec corpus.");
+            url = new URL(CORPUS_URL); //File Location goes here
+            con = url.openConnection(); // open the url connection.
+            dis = new DataInputStream(con.getInputStream());
+            fileData = new byte[con.getContentLength()];
+            for (int q = 0; q < fileData.length; q++) {
+                fileData[q] = dis.readByte();
+            }
+            dis.close(); // close the data input stream
+            fos = new FileOutputStream(new File(CORPUS_FILE_ROUTE)); //FILE Save Location goes here
+            fos.write(fileData);  // write out the file we want to save.
+            fos.close(); // close the output stream writer
+        }
+        catch(Exception m) {
+            System.out.println(m);
+        }
+
+
+    }
+
+    private Word2VecHandler() {
+    }
+}
